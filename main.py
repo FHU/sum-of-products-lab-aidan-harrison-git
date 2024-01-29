d5f5cff5b21e9848032e36b554a187d44b62c90e
aidan-harrison-git <aidan.harrison@students.fhu.edu>
2024-01-29 17:35:11 -0600

    update main.py
    help?

--- main.py
+++ main.py
@@ -10,8 +10,13 @@ def sum_of_products(list1, list2):
 list1 = [] 
 list2 = []  
 
-list1 = [int(digit) for digit in input("Provide a series of integers with no spaces in between each: ")]
-list2 = [int(digit) for digit in input("Provide another series of the same length and format: ")]
+# list1 = [int(digit) for digit in input("Provide a series of integers with no spaces in between each: ")]
+# list2 = [int(digit) for digit in input("Provide another series of the same length and format: ")]
+
+for digit in input("Provide a series of integers with no spaces in between each: "):
+    list1.append(int(digit))
+for digit in input("Provide another series of the same length and format: "):
+    list2.append(int(digit))
 
 if len(list1) != len(list2):
     print('Error: each series must be the same length')
