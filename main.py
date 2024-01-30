d1ac60331c37f8c75718b30e1371c7f2aa0385ba
aidan-harrison-git <aidan.harrison@students.fhu.edu>
2024-01-30 12:50:05 -0600

    update main.py
    magic function added

--- main.py
+++ main.py
@@ -6,22 +6,22 @@ def sum_of_products(list1, list2):
         total += (number*list2[counter])
         counter += 1
     return total
+if __name__ == '__main__':
+    list1 = [] 
+    list2 = []  
 
-list1 = [] 
-list2 = []  
+    # list1 = [int(digit) for digit in input("Provide a series of integers with no spaces in between each: ")]
+    # list2 = [int(digit) for digit in input("Provide another series of the same length and format: ")]
 
-# list1 = [int(digit) for digit in input("Provide a series of integers with no spaces in between each: ")]
-# list2 = [int(digit) for digit in input("Provide another series of the same length and format: ")]
+    for digit in input("Provide a series of integers with no spaces in between each: "):
+        list1.append(int(digit))
+    for digit in input("Provide another series of the same length and format: "):
+        list2.append(int(digit))
 
-for digit in input("Provide a series of integers with no spaces in between each: "):
-    list1.append(int(digit))
-for digit in input("Provide another series of the same length and format: "):
-    list2.append(int(digit))
-
-if len(list1) != len(list2):
-    print('Error: each series must be the same length')
-else:
-    print(sum_of_products(list1, list2))
+    if len(list1) != len(list2):
+        print('Error: each series must be the same length')
+    else:
+        print(sum_of_products(list1, list2))
 
 
 
