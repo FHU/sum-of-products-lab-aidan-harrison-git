7781cbd06391fe98aedeef979589c2e00067b363
aidan-harrison-git <aidan.harrison@students.fhu.edu>
2024-01-31 11:52:54 -0600

    main.py
    put error message in function instead of if statement

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 def sum_of_products(list1, list2):
     counter = 0
     total = 0
-    for number in list1:
-        
-        total += (number*list2[counter])
-        counter += 1
+    if len(list1) != len(list2):
+        raise ValueError('Error - series 1 and 2 must be equal in length')
+    else:
+        for number in list1:
+            total += (number*list2[counter])
+            counter += 1
     return total
 
 if __name__ == '__main__':
@@ -22,10 +24,7 @@ if __name__ == '__main__':
     for digit in list2str:
         list2.append(int(digit))
 
-    if len(list1) != len(list2):
-        raise ValueError('Error - series 1 and 2 must be equal in length')
-    else:
-        print(sum_of_products(list1, list2))
+    print(sum_of_products(list1, list2))
 
 
 # read in the strings first
